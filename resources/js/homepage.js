d024ba74e33aa9904c590a1a25713fb4cec6fc75
rizkimn <rizkimnur0@gmail.com>
2025-09-08 00:16:47 +0900

    update homepage.js add some route by leaflet-routing-machine

--- resources/js/homepage.js
+++ resources/js/homepage.js
@@ -1,6 +1,8 @@
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import { leafletProviders } from './leaflet-providers';
+import 'leaflet-routing-machine';
+import 'leaflet-routing-machine/dist/leaflet-routing-machine.css';
 import userMarker from "/public/assets/img/leaflet/user-marker.png";
 
 const map = L.map('map', {
@@ -8,34 +10,21 @@ const map = L.map('map', {
     maxZoom: 18
 }).setView([0.6973449319031846, 127.95485842430058], 9);
 
-// L.control.zoom({
-//     position: 'bottomleft'
-// }).addTo(map);
-
 L.tileLayer(leafletProviders['Stadia']['StamenTerrain']['url'], {
     attribution: leafletProviders['Stadia']['StamenTerrain']['html_attribution'],
 }).addTo(map);
 
-const locations = window.locationsData
+const locations = window.locationsData;
 
-locations.forEach(loc => {
-    const lat = loc.latitude;  // langsung pakai kolom latitude
-    const lng = loc.longitude; // langsung pakai kolom longitude
-
-    L.marker([lat, lng])
-        .addTo(map)
-        .bindPopup(`<b>${loc.name}</b>`)
-        .on("click", () => {
-            map.setView([lat, lng], 16, { animate: true });
-        })
-});
+// variabel global untuk user location & rute
+let userLatLng = null;
+let routingControl = null;
 
 map.locate({ setView: true, maxZoom: 12 });
 
-map.on('locationfound', function(e) {
-    const userLatLng = e.latlng;
+map.on('locationfound', function (e) {
+    userLatLng = e.latlng;
 
-    // Marker user dengan ikon custom (biru misalnya)
     const userIcon = L.icon({
         iconUrl: userMarker,
         iconSize: [60, 60],
@@ -46,13 +35,48 @@ map.on('locationfound', function(e) {
     L.marker(userLatLng, {
         icon: userIcon,
         zIndexOffset: 666
-    })
-    .addTo(map)
-    .on("click", () => {
-        map.setView(userLatLng, 18, { animate: true });
-    });
+    }).addTo(map)
+        .on("click", () => {
+            map.setView(userLatLng, 18, { animate: true });
+        });
 });
 
-map.on('locationerror', function() {
+map.on('locationerror', function () {
     alert("Tidak bisa mendapatkan lokasi Anda 😢");
+});
+
+// buat marker lokasi wisata
+locations.forEach(loc => {
+    const lat = loc.latitude;
+    const lng = loc.longitude;
+
+    L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(`<b>${loc.name}</b>`)
+        .on("click", () => {
+            if (!userLatLng) {
+                alert("Lokasi Anda belum terdeteksi 🚫");
+                return;
+            }
+
+            // hapus rute lama kalau ada
+            if (routingControl) {
+                map.removeControl(routingControl);
+            }
+
+            // buat rute baru user -> lokasi ini
+            routingControl = L.Routing.control({
+                waypoints: [
+                    L.latLng(userLatLng.lat, userLatLng.lng),
+                    L.latLng(lat, lng)
+                ],
+                lineOptions: {
+                    styles: [{ color: '#E21C51', weight: 4 }]
+                },
+                addWaypoints: false,
+                draggableWaypoints: false,
+                fitSelectedRoutes: true,
+                showAlternatives: false
+            }).addTo(map);
+        });
 });
